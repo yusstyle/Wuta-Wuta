456e1dc259577d3469f42effd32103b69b24e0bf
SamixYasuke <samueladekolu4@gmail.com>
2026-03-24 20:52:14 +0100

    added prisma schema adn updated README.md to show how t setup db

---
+++ server/prisma.js
@@ -0,0 +1,7 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+});
+
+module.exports = prisma;
